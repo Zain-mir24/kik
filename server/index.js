9277e5f6acc44b372a12582813c5e222a3c79b81
Zainmir <zainnaeemk10@gmail.com>
2021-03-22 15:03:19 +0000

    creating front end

--- server/index.js
+++ server/index.js
@@ -2,3 +2,8 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socketio');
 const router = require('./router')
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+app.use(cors());
+app.use(router)
