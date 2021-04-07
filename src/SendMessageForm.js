91c871865d05ec10ee6087f0c80c1d842c92a3b7
Zainmir <zainnaeemk10@gmail.com>
2021-04-07 14:56:23 +0000

    fixing the components of the program

--- src/SendMessageForm.js
+++ src/SendMessageForm.js
@@ -1,28 +1,37 @@
+import React, { useState } from "react";
 
-import React from 'react'
+import { Link } from "react-router-dom";
+const styleinput = {
+  width: "200px",
+  borderRadius: "20px",
+};
+const buttonstyle = {
+  color: "white",
+  backgroundColor: "#42a5f5",
+  height: "50px",
+  width: "100px",
+  marginTop: "10px",
+};
+function SendMessageForm({message, setMessage,sendMessage}) {
+ 
+  return (
+    <div>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={(e) => e.key=='Enter'?sendMessage(e):null}
+        style={styleinput}
+      />
+      <button>Enter</button>
+      <div>
+        <Link to="/">
+          {" "}
+          <button style={buttonstyle} onClick={(e)=>sendMessage(e)}>Exit the app</button>{" "}
+        </Link>
+      </div>
+    </div>
+  );
+}
 
-import {Link } from 'react-router-dom'
-const styleinput={
-    width:"200px",
-    borderRadius:"20px"
-}
-const buttonstyle={
-    color:"white",
-    backgroundColor:"#42a5f5",
-    height:"50px",
-    width:"100px",
-    marginTop:"10px"
-}
-function SendMessageForm() {
-    return (
-        <div>
-            <input type="text" style={styleinput}/>
-            <button>Send message</button>
-            <div>
-            <Link to="/">   <button style={buttonstyle}>Exit the app</button> </Link> 
-            </div>
-        </div>
-    )
-}
-
-export default SendMessageForm
+export default SendMessageForm;
